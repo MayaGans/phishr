03e75ad85ed32481ad709c3c62569eda56b6753e
Maya <jaffe.maya@gmail.com>
2020-03-29 18:07:16 -0700

    removing old phishnet setlist algo

--- R/phishnet_get_setlist.R
+++ R/phishnet_get_setlist.R
@@ -1,42 +1,10 @@
-#' @title Query the Phish.Net API
-#'
-#' @description The \code{phish.net} API contains a vast amount of information.
-#' The functions here are specific to each type of query that can be sent to the
-#' API - all are prefixed with \code{pn_*}
-#'
-#' @param apikey Your key for the Phish.net API. This can also be stored
-#' as an option using \code{options('phishnet_key') <- 'your_api_key'}. See details
-#' for more information on how to obtain one.
-#' @param showdate the show setlist in YYYY-MM-DD format
-#'
-#' @details Accessing the Phish.Net API requires an API key. These can be obtained
-#' by following the steps at \href{http://api.phish.net/keys/}{Phish.net API keys}.
-#' First, you'll need to create a username and password. After confirming your
-#' this information, you can follow the link above to create an API key. Once
-#' approved, store the private key somewhere that it is easily retrieved, and
-#' use it in the \code{apikey} argument to each \code{pn_*} function. Alternatively,
-#' you can set a \code{option(phishnet_key = '<your-key'>)} in your .Rprofile.
-#'
-#' @return the selected show's rating, setlist, or notes as a \code{data.frame}.
-#'
-#' @examples
-#' \dontrun{
-#' BigCypressNYE <- pn_get_setlist(apikey = "<apikey>", showdate = "1999-12-31")
-#' }
-#'
-#' @importFrom attempt stop_if_all
-#' @importFrom purrr compact
-#' @importFrom jsonlite fromJSON
-#' @importFrom httr GET content
-#' @import jsonlite
-#' @importFrom textreadr read_html
-#' @importFrom dplyr filter
-#' @importFrom stringr str_split_fixed
-#' @importFrom zoo na.locf
-#'
-#' @export
 #' @rdname phish_dot_net
-
+#'
+#' @importFrom xml2 read_html xml_find_all as_list
+#' @importFrom stringr regex str_detect
+#' @importFrom purrr map2
+#' @importFrom magrittr %>%
+#' @export
 
 pn_get_setlist <- function(apikey = getOption('phishnet_key'),
                            showdate = NULL) {
@@ -56,7 +24,7 @@ pn_get_setlist <- function(apikey = getOption('phishnet_key'),
       "&showdate=",
       showdate,
       sep = "")
-    )
+  )
 
   # Check the result
   check_status(res)
@@ -67,71 +35,95 @@ pn_get_setlist <- function(apikey = getOption('phishnet_key'),
 
   if (length(cont$response$data) > 0) {
 
-    # cont$response$data[[1]]$setlistdata
-    # contains the setlist data in html which is a hot mess
-    # cont$response$data[[1]]$setlistdata
-    # contains the setlist data in html which is a hot mess
-    set_html <- textreadr::read_html(cont$response$data[[1]]$setlistdata)
+    xml_show <- xml2::read_html(cont$response$data[[1]]$setlistdata)
 
-    # combine the colon with the previous line
-    # because colons are found with "Set 1:", "Set 2:" etc.
-    ind.colon <- which(set_html == ":")
-    ind.set <- ind.colon - 1
-
-    # paste the colon and the prior line together
-    # to concatenate Set 1 and :
-    set_html[ind.set] <- paste(set_html[ind.set], set_html[ind.colon], sep = "")
-
-    # make into data frame
-    set_html <- data.frame(set_html)
-    # call the song column Song
-    set_html$Song <- as.character(set_html$set_html)
-    set_html$set_html <- NULL
-
-    # remove cells containing brackets, this is a vestage of show notes
-    # remove cells only containing special characters
-    set_html <- dplyr::filter(set_html, !grepl("\\[",set_html$Song))
-    set_html <- dplyr::filter(set_html, !grepl("^:$",set_html$Song))
-
-    # create a character string for song names
-    # the easiest filter is that they should include all alphabetical characters
-    # but then we also need to search for the songs that are numbers
-    # call others false
-    boo <- c("[a-zA-Z]+", "1999", "555", "5:15")
-
-    # now we can set our songs to true and the rest to false
-    set_html$Boolian <- grepl(paste(boo, collapse = "|"), set_html$Song)
-
-    # now we can take the cells that were returned as false
-    # and put those into a new column for segues
-    ind.FALSE <- which(set_html$Boolian == FALSE)
-    ind.CAT <- ind.FALSE - 1
-
-    set_html$Song[ind.CAT] <- paste(set_html$Song[ind.CAT], set_html$Song[ind.FALSE], sep = "__")
-    set_html <- filter(set_html, set_html$Boolian == TRUE)
-    set_html <- stringr::str_split_fixed(set_html$Song, "__", 2)
-    colnames(set_html) <- c("Song", "Segue")
-
-    set_html <- data.frame(set_html)
-
-    # now we can remove the rows containing the set
-    # and use that data to populate which set each show belongs in
-    # we will use a combination of a colon and the colon being
-    # the last character in the string (because of RS song 5:15)
-
-    substrRight <- function(x, n){
-      substr(x, nchar(x)-n+1, nchar(x))
-    }
-
-    set_html$Set <- ifelse(substrRight(as.character(set_html$Song), 1) == ":", paste(set_html$Song), NA)
-    set_html$Set <- zoo::na.locf(set_html$Set)
-    set_html <- filter(set_html, set_html$Song != set_html$Set)
-
+    out <- .clean_pn_setlist(xml_show)
 
   } else {
     # in the case that the show had no data set that list element to NA
-    set_html <- NA
+    out <- NA
   }
-  return(set_html)
+  return(out)
 }
 
+
+#' @noRd
+# This uses xml nodes to find the set list text.
+# It then removes all of the [0-9] stuff that indicate notes,
+# creates a vector for each set, then merges the text that separates each
+# song into a single line with the song (e.g. Chalkdust -> Ass Handed, becomes
+# something like tribble(~song, ~ seg,
+#                        'Chalkdust', '->',
+#                        'Ass Handed', ','))
+
+.clean_pn_setlist <- function(xml_show) {
+
+  ugly_mess <- xml2::xml_find_all(xml_show, xpath = '//text()') %>%
+    xml2::as_list()
+
+  # These are mostly show notes, can be added later when as needed (famous last words)
+  toss_out  <- stringr::regex('[\\[0-9]\\]|\\:')
+
+  # create list index for shit we don't want, keep the stuff we do
+
+  toss_ind <- vapply(ugly_mess,
+                     function(x, pat) stringr::str_detect(x, pat),
+                     logical(1L),
+                     pat = toss_out)
+
+  less_mess <- ugly_mess[!toss_ind]
+
+  # Now, find the set splits, and create a list with a vector for each one.
+
+  set_pat   <- stringr::regex('Set [0-9]|Encore')
+
+  set_ind <- vapply(less_mess,
+                    function(x, pat) stringr::str_detect(x, pat),
+                    logical(1L),
+                    pat = set_pat) %>%
+    which()
+
+  sets <- list()
+
+  for(i in seq_along(set_ind)) {
+
+    start <- set_ind[i] + 1
+
+    end <- ifelse(i == length(set_ind),
+                  length(less_mess),
+                  set_ind[(i + 1)] - 1)
+
+    sets[[i]] <- unlist(less_mess[start:end])
+    names(sets)[i] <- gsub(' ', '_', less_mess[set_ind[i]])
+
+  }
+
+  # As far as a I can tell, the pattern is always song segue song segue.
+  # so odd indices correspond to songs, and the index after is always the segue.
+  # the exception is the final song of the set - that never has a segue
+
+  temp <- purrr::map2(.x = sets,
+                      .y = names(sets),
+                      function(.x, .y) {
+                        song_ind <- seq(1, length(.x), by = 2)
+                        seg_ind  <- song_ind[-length(song_ind)] + 1
+
+                        out <- data.frame(Set = .y,
+                                          Song = .x[song_ind],
+                                          Segue = c(.x[seg_ind], NA_character_),
+                                          stringsAsFactors = FALSE)
+                        return(out)
+
+                      })
+
+  # Now some housekeeping to create the returnable dataframe
+
+  temp$make.row.names <- FALSE
+  out <- do.call('rbind', temp)
+  out$Set <- gsub('_', ' ', out$Set)
+
+  # out$Segue[grepl(',', out$Segue)] <- NA
+
+  return(out)
+
+}
